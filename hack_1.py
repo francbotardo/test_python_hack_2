0ed61dd2c8bfee17c3d6d765f9abdb890179fd94
phantomve <francisco_cas14@hotmail.com>
2023-11-27 19:13:16 -0400

    feat:updating file hack_1.py

--- hack_1.py
+++ hack_1.py
@@ -7,6 +7,13 @@ text: "eq" output => "eq"
 """
 
 
-def fn_hack_1():
-    result = "fooziman"
-    return result
+def fn_hack_1(texto):
+    if len(texto) <= 2:
+        return texto
+    else:
+        texto = texto[0] + texto[1].upper() + texto[2:]
+        if len(texto) >= 5:
+            texto = texto[:4] + texto[4].upper() + texto[5:]
+            return texto
+        else:
+            return texto
